f61b5de502f1ede2e8d5cbc2938c17806f2d3628
UllasSG <110962215+UllasSG@users.noreply.github.com>
2022-12-18 12:22:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ def addplayer(team):
     team[name]={'points':0,'assists':0,'FGA':0,'FGM':0,'3PA':0,'3PM':0}
 
 
-<<<<<<< HEAD
-
-
-=======
 def FGp(team,player):#returns Field goal % as int
     if (team[player]['FGA'])==0:
         return 0
     fgp=((team[player]['FGM'])/(team[player]['FGA']))*100
     return fgp
->>>>>>> 4cbcc654a69253a817fe04bb7eb89b6b2ba6a829
+
