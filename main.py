cf768701512025b2c46b6fe2bfd21b7a466418e9
UllasSG <110962215+UllasSG@users.noreply.github.com>
2022-12-18 11:52:47 +0530

    Update main.py
    made changes to add player

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-n=3
 home={}
-print('HOME TEAM')
-for i in range(n):
-    nameh=input('ENTER PLAYER NAME')
-    home[nameh]={'points':0,'assists':0,'FGA':0,'FGM':0,'3PA':0,'3PM':0}
-
-
 away={}
-print('AWAY TEAM')
-for i in range(n):
-    namea=input('ENTER PLAYER NAME')
-    away[namea]={'points':0,'assists':0,'FGA':0,'FGM':0,'3PA':0,'3PM':0}
+def addplayer(team):
+    name=input('ENTER NAME')
+    team[name]={'points':0,'assists':0,'FGA':0,'FGM':0,'3PA':0,'3PM':0}
 
-print(home)
-print(away)
